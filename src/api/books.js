23e109acd6040b9ebbd86f48d15811f6a569b426
bbworld1 <vwangsf@gmail.com>
2019-12-29 01:31:41 -0800

    Fix bugs and add default 'Unknown' strings for fields

--- src/api/books.js
+++ src/api/books.js
@@ -16,24 +16,29 @@ function search(query) {
    * }
    */
   return new Promise((done, error) => {
+    console.log("Searching for book...")
     bookifyCache.search(query)
-    .then((results) => {
-      let books = [];
-      console.log(get)
-      for (let result of results) {
-        books.push({
-          title: result.title,
-          authors: result.authors,
-          publisher: result.publisher,
-          date: result.publishedDate,
-          categories: result.categories,
-          thumbnail: result.thumbnail,
-          pages: result.pageCount
-        });
-      }
-      done(books);
-    });
+      .then((results) => {
+        let books = [];
+        for (let result of results) {
+          books.push({
+            title: result.title,
+            authors: result.authors == undefined ? "Unknown Author": result.authors,
+            publisher: result.publisher == undefined ? "Unknown Publisher" : result.publisher,
+            date: result.publishedDate == undefined ? "Published Date Unknown": result.publishedDate,
+            categories: result.categories,
+            thumbnail: result.thumbnail,
+            pages: result.pageCount == undefined ? "Unknown page count" : result.pageCount
+          });
+        }
+        done(books);
+      })
+      .catch((err) => {
+        console.log(err);
+        error(err);
+      });
   });
 }
+
 
 module.exports.search = search;
